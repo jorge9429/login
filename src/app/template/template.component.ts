1dd0d01a14aa099fb66f643bfadc6270e290ea0a
Jorge Reascos <jorge_9429@hotmail.com>
2019-01-20 09:37:30 -0500

    TemplateStructureCorrection
    Jorge Reascos

--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -5,11 +5,125 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './template.component.html',
   styleUrls: ['./template.component.css']
 })
-export class TemplateComponent implements OnInit {
+export class TemplateComponent  {
+  menuMode = 'static';
 
-  constructor() { }
+   topbarMenuActive: boolean;
 
-  ngOnInit() {
-  }
+   overlayMenuActive: boolean;
 
+   staticMenuDesktopInactive: boolean;
+
+   staticMenuMobileActive: boolean;
+
+   layoutMenuScroller: HTMLDivElement;
+
+   lightMenu = true;
+
+   lightTopbar = true;
+
+   topbarColor = 'layout-topbar-bluegrey';
+
+   menuClick: boolean;
+
+   topbarItemClick: boolean;
+
+   resetMenu: boolean;
+
+   menuHoverActive: boolean;
+
+   onLayoutClick() {
+       if (!this.topbarItemClick) {
+           this.topbarMenuActive = false;
+       }
+
+       if (!this.menuClick) {
+           if (this.isHorizontal() || this.isSlim()) {
+               this.resetMenu = true;
+           }
+
+           if (this.overlayMenuActive || this.staticMenuMobileActive) {
+               this.hideOverlayMenu();
+           }
+
+           this.menuHoverActive = false;
+       }
+
+       this.topbarItemClick = false;
+       this.menuClick = false;
+   }
+
+   onMenuButtonClick(event) {
+       this.menuClick = true;
+       this.topbarMenuActive = false;
+
+       if (this.isOverlay()) {
+           this.overlayMenuActive = !this.overlayMenuActive;
+       }
+       if (this.isDesktop()) {
+           this.staticMenuDesktopInactive = !this.staticMenuDesktopInactive;
+       } else {
+           this.staticMenuMobileActive = !this.staticMenuMobileActive;
+       }
+
+       event.preventDefault();
+   }
+
+   onMenuClick($event) {
+       this.menuClick = true;
+       this.resetMenu = false;
+   }
+
+   onTopbarMenuButtonClick(event) {
+       this.topbarItemClick = true;
+       this.topbarMenuActive = !this.topbarMenuActive;
+
+       this.hideOverlayMenu();
+
+       event.preventDefault();
+   }
+
+   onTopbarSubItemClick(event) {
+       event.preventDefault();
+   }
+
+   isHorizontal() {
+       return this.menuMode === 'horizontal';
+   }
+
+   isSlim() {
+       return this.menuMode === 'slim';
+   }
+
+   isOverlay() {
+       return this.menuMode === 'overlay';
+   }
+
+   isStatic() {
+       return this.menuMode === 'static';
+   }
+
+   isMobile() {
+       return window.innerWidth < 1025;
+   }
+
+   isDesktop() {
+       return window.innerWidth > 1024;
+   }
+
+   isTablet() {
+       const width = window.innerWidth;
+       return width <= 1024 && width > 640;
+   }
+
+   hideOverlayMenu() {
+       this.overlayMenuActive = false;
+       this.staticMenuMobileActive = false;
+   }
+
+   changeMenuMode(menuMode: string) {
+       this.menuMode = menuMode;
+       this.staticMenuDesktopInactive = false;
+       this.overlayMenuActive = false;
+   }
 }
